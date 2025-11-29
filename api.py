3d8328c380ce4184cae172dee556d0269d1f43d3
Jared <jaredawiti2017@gmail.com>
2025-11-29 21:04:53 +0000

    Create API and starwars ship ordering

--- api.py
+++ api.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI, HTTPException
+import info_retriever
 
 ## API for Star Wars
+
+app = FastAPI()
+
+@app.get('/starships')
+def get_starships_req():
+    return info_retriever.display_ordered_starships_by_name_asc()
---
+++ info_retriever.py
@@ -0,0 +1,33 @@
+import requests
+
+STARSHIPS_API = "https://swapi.py4e.com/api/starships"
+
+def get_all_starships():
+    all_responses = []
+    for x in range(1,5):
+        repsonse = requests.get(STARSHIPS_API+f"?page={x}").json()
+        for starship in repsonse["results"]:
+            all_responses.append(starship)
+    return all_responses
+
+def order_starships_by_name(all_starships, direction):
+    if direction == 'asc':
+        ordered_starships_by_name = sorted(all_starships, key=lambda k: k["name"])
+    elif direction == 'desc':
+        ordered_starships_by_name = sorted(all_starships, key=lambda k: k["name"], reverse=True)
+    return ordered_starships_by_name
+
+def display_ordered_starships_by_name_asc():
+    all_starships = get_all_starships()
+    ordered_starships = order_starships_by_name(all_starships, 'asc')
+    return ordered_starships
+
+def display_ordered_starships_by_name_desc():
+    all_starships = get_all_starships()
+    ordered_starships = order_starships_by_name(all_starships, 'desc')
+    return ordered_starships
+
+if __name__ == "__main__":
+    #print(get_all_starships())
+    all_starships = get_all_starships()
+    ordered_starships = order_starships_by_name(all_starships)
